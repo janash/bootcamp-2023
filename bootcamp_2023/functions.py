af2302b678bebce728a8fa3fb79582e50b9e1f23
Jessica Nash <janash@vt.edu>
2023-07-18 10:59:19 -0400

    add hello_world quote

--- bootcamp_2023/functions.py
+++ bootcamp_2023/functions.py
@@ -23,6 +23,15 @@ def canvas(with_attribution=True):
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
 
+def hello_world(with_attribution=True):
+
+    quote = "Hello, world!"
+
+    if with_attribution:
+        quote += "\n\t - Jessica Nash"
+
+    return quote
+
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
